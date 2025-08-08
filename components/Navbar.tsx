c86e45e8bf5f7822c33063191df274381e181083
Ayush Gupta <ayushgupta3902@gmail.com>
2025-08-08 23:15:41 +0530

    feat: setup the project and implement navbar and hero section

---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { navLinks } from "@/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: "nav",
+      start: "bottom top",
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  }, []);
+
+  return (
+    <nav>
+      <div>
+        <Link href={"#home"} className="flex items-center gap-2">
+          <p>Urban ठेका</p>
+          <Image
+            src={"/images/logo.png"}
+            alt="logo"
+            height={100}
+            width={100}
+            className="object-cover size-9"
+          />
+        </Link>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <Link href={`#${link.id}`}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ lib/gsap.ts
@@ -0,0 +1,6 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export { gsap, ScrollTrigger, SplitText };
